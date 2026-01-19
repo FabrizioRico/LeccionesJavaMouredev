6744d8040555d6b480d075c5abb73a4f805a8cfc
FabrizioRico <fabrizioriscoalva05@gmail.com>
2026-01-19 17:41:53 -0500

    Primera leccion desarrollada

---
+++ src/leccion/saludo/ClaseSaludo.java
@@ -0,0 +1,17 @@
+/*
+ * ClaseSaludo
+ * Clase que imprime un saludo en la consola
+ * 
+ * Creado por Fabrizio Risco Alva
+ * Creado en: 16/01/26
+ * 
+ * */
+
+package leccion.saludo;
+
+public class ClaseSaludo {
+	public static void main(String[] args) {
+		System.out.println("Hola Eclipse");
+	}
+
+}
---
+++ src/leccion/variablesConstantes/ClaseVarCon.java
@@ -0,0 +1,7 @@
+package leccion.variablesConstantes;
+
+public class ClaseVarCon {
+	public static void main(String[] args) {
+		
+	}
+}
